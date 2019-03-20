bef7f0ebccd763f7d0a7f44648d625579f0a25c0
didier-tp <dd@ici-ou-labas.fr>
2019-03-20 12:41:47 +0100

    v2

--- tpFramework/src/main/java/fr/afcepf/al33/tp/test/TestReflectApp.java
+++ tpFramework/src/main/java/fr/afcepf/al33/tp/test/TestReflectApp.java
@@ -1,22 +1,37 @@
 package fr.afcepf.al33.tp.test;
 
 import java.lang.reflect.Method;
+import java.util.Date;
 
 public class TestReflectApp {
-
-	public static void main(String[] args) throws Exception {
-
-        //String className="java.util.Date";
+	
+	public static void enPartantNomClasse() throws Exception {
+		//String className="java.util.Date";
         //String className="java.sql.DriverManager";
 		String className="java.lang.Math";
         
         Class<?> metaClasse = Class.forName(className);
         
-        System.out.println("liste des méthodes de la classe:");
+        System.out.println("liste des méthodes de la classe " + metaClasse.getName());
         for(Method m : metaClasse.getDeclaredMethods() ) {
         	System.out.println("\t" + m.getName());
         }
+	}
+	
+	public static void enPartantInstance() throws Exception {
+		Date date = new Date();//java.util
+        
+        Class<?> metaClasse = date.getClass();
+        
+        System.out.println("liste des méthodes de la classe " + metaClasse.getName());
+        for(Method m : metaClasse.getDeclaredMethods() ) {
+        	System.out.println("\t" + m.getName());
+        }
+	}
 
+	public static void main(String[] args) throws Exception {
+		enPartantNomClasse();
+		enPartantInstance();
 	}
 
 }
