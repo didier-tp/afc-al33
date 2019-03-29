743cbcb5f99edc0786343a3db3d88d53acba263e
didier-tp <dd@ici-ou-labas.fr>
2019-03-29 15:12:08 +0100

    avec persistence.xml

--- tpSpringD/tpSpringDCore/src/main/java/fr/afcepf/al33/tp/entity/Client.java
+++ tpSpringD/tpSpringDCore/src/main/java/fr/afcepf/al33/tp/entity/Client.java
@@ -1,12 +1,20 @@
 package fr.afcepf.al33.tp.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter @Setter @ToString @NoArgsConstructor
+@Entity
 public class Client {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long numero;
 	private String nom;
 	//...
