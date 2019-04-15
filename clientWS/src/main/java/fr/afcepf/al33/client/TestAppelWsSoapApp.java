7cda6330664e823f86879225b412da7666151d0c
didier-tp <dd@ici-ou-labas.fr>
2019-04-15 16:53:19 +0200

    avec main appelant WS soap via proxy

--- clientWS/src/main/java/fr/afcepf/al33/client/TestAppelWsSoapApp.java
+++ clientWS/src/main/java/fr/afcepf/al33/client/TestAppelWsSoapApp.java
@@ -1,14 +1,21 @@
 package fr.afcepf.al33.client;
 
+import fr.afcepf.al33.conv.Convertisseur;
+import fr.afcepf.al33.conv.ConvertisseurImplService;
+
 public class TestAppelWsSoapApp {
 
 	
-
+    //à lancer via Run as / java application
 	public static void main(String[] args) {
 		//code d'appel du WS soap qui s'appuie sur le code généré
 		//pas wimport -keep -... URL_WSDL
 		//.... (...Service).get...Port() 
-
+        Convertisseur proxyWsConvertisseur
+        = (new ConvertisseurImplService()).getConvertisseurImplPort();
+        
+        System.out.println("15 euros=" + 
+            proxyWsConvertisseur.euroToFranc(15.0));
 	}
 
 }
