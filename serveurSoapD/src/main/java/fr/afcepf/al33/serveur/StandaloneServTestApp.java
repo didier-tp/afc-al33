a1f8ef5a9164f3832bee91de9c33e705fc5f8f70
didier-tp <dd@ici-ou-labas.fr>
2019-04-15 12:31:45 +0200

    avec démarrage serveurSoap

---
+++ serveurSoapD/src/main/java/fr/afcepf/al33/serveur/StandaloneServTestApp.java
@@ -0,0 +1,29 @@
+package fr.afcepf.al33.serveur;
+
+import javax.xml.ws.Endpoint;
+
+import fr.afcepf.al33.conv.ConvertisseurImpl;
+
+public class StandaloneServTestApp {
+
+	public StandaloneServTestApp() {
+		System.out.println("demarrage du serveurSoap ...");
+		String URL="http://localhost:8087/conv/Convertisseur";//url du endpoint soap
+		ConvertisseurImpl implementor= new ConvertisseurImpl();
+		System.out.println("URL WSDL="+URL+"?wsdl");
+		Endpoint.publish(URL, implementor);//ça démarre un mini container web/http
+		//intégré à la machine virtuelle java (ressemblant à Jetty)
+	}
+
+	public static void main(String[] args) {
+		new StandaloneServTestApp();//thread en tache de fond pour traiter requetes
+		try {
+			Thread.sleep(1000 * 60 * 15); //pause de 15 minutes
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("arret automatique du serveur au bout de 15 minutes");
+		System.exit(0);//arret complet (de tous les Threads) du serveur
+	}
+
+}
